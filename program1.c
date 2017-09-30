b952d63b5166d9a45c9d205e623609cd49eb34fc
kalima007c <kalima007c@gmail.com>
2017-09-30 22:46:48 +0700

    program1 bugged

---
+++ program1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void c(void);			// Function Prototype
+int x = 1;				// Global Variable
+int main()
+{
+		int i = 5;		// Local to main
+		while(i < 100)
+		c();
+		return 0;		
+}
+void c()
+{
+	x *=1;
+	printf("%d",x);
+}
