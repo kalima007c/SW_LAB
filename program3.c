e5d10d8e58397ca15ce4c7f041abe68ec5d06ff3
kalima007c <kalima007c@gmail.com>
2017-10-01 01:50:30 +0700

    program3 debug

---
+++ program3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#define DEBUG 0 
+void c(void);
+int x = 1;
+int ca=0;
+FILE *fp;
+
+int main()
+{
+	fp = fopen("data.log","w+");
+	// int x = 5;
+	if (DEBUG)
+		fprintf(fp,"x = %d",x);
+	while(x < 100)
+	{	
+		c();
+		if (DEBUG) fprintf(fp,"x = %d",x);
+		x++;
+	}
+	fclose(fp);
+	return 0;
+}
+void c(void)
+{
+	x *= 1.0;
+	fprintf(fp,"x = %d\n",x);
+}
